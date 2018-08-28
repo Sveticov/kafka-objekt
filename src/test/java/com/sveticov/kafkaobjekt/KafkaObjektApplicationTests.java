326d0de5cd828deb0cb44d92fc18744b34b49ff9
Sveticov <sveticovdanil@gmail.com>
2018-08-28 22:06:36 +0300

    6

---
+++ src/test/java/com/sveticov/kafkaobjekt/KafkaObjektApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sveticov.kafkaobjekt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KafkaObjektApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
